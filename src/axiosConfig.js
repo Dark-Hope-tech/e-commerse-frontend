8b280a2b2f376071ac982a3e425d4ed12c919a26
Tabish Malik <76650437+Dark-Hope-tech@users.noreply.github.com>
2024-06-20 23:22:01 +0530

    added domain

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-const baseURL = process.env.BASE_URL;
-console.log(baseURL);
+const baseURL = 'https://e-commerce-backend-f254.onrender.com';
 axios.defaults.baseURL =
   process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : baseURL;
-console.log(process.env.NODE_ENV);
